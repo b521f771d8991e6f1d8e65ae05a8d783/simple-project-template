5fb048d8dfc3734b47cf67a56f5626415ef9d774
Alexander M. Hipfl <7h3h4qdn@duck.com>
2026-04-07 06:01:12 +0000

    added native and rust part

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay, crane }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -13,7 +15,10 @@
     {
       packages = forAllSystems (system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
           version = self.shortRev or self.dirtyShortRev or "dev";
 
           # Read PROJECT_NAME from .env
@@ -21,6 +26,49 @@
           projectName = let
             match = builtins.match ".*PROJECT_NAME=([^\n]+).*" envFile;
           in if match != null then builtins.head match else "simple-project-template";
+
+          muslTarget = if system == "aarch64-linux" || system == "aarch64-darwin"
+            then "aarch64-unknown-linux-musl"
+            else "x86_64-unknown-linux-musl";
+
+          # Rust toolchain with musl and wasm targets
+          rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+            targets = [
+              "x86_64-unknown-linux-musl"
+              "aarch64-unknown-linux-musl"
+              "wasm32-unknown-unknown"
+            ];
+          };
+
+          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+          rustSrc = pkgs.lib.cleanSourceWith {
+            src = ./.;
+            filter = path: type:
+              (craneLib.filterCargoSources path type);
+          };
+
+          commonArgs = {
+            src = rustSrc;
+            pname = projectName;
+            inherit version;
+          };
+
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+          # musl static build
+          rustMusl = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+            CARGO_BUILD_TARGET = muslTarget;
+            CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+          });
+
+          # wasm build
+          rustWasm = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+            CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+            doCheck = false;
+          });
         in
         rec {
           # do not add more targets, use this to build everything
@@ -35,18 +83,29 @@
 
             nativeBuildInputs = with pkgs; [
               git
-            ];
-            buildPhase = ''
-              export HOME=$TMPDIR
-              export EXPO_NO_TELEMETRY=1
 
+              # Objective C/++ Toolchain
+              clang
+              cmake
+              ninja
+              gnustep-base
+
+              # Rust toolchain with musl + wasm targets
+              rustToolchain
+              wasm-bindgen-cli_0_2_114
+            ];
+
+            env = {
+              EXPO_NO_TELEMETRY=1;
+            };
+
+            buildPhase = ''
               echo "${version}" > VERSION
 
               npm run build:node
             '';
 
             installPhase = ''
-              mkdir -p $out/bin
               cp -r dist/* $out/bin/
             '';
 
@@ -61,7 +120,7 @@
             contents = [
               pkgs.busybox
             ];
-            
+
             config = {
               Cmd = [ "${default}/bin/${default.meta.mainProgram}" ];
               ExposedPorts = { "8081/tcp" = {}; };
---
+++ src-rust/lib.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![forbid(unsafe_code)]
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
+}
