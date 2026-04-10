aaf50e2762bff4fbb0bbd18272691fe420dabad4
Alexander M. Hipfl <7h3h4qdn@duck.com>
2026-04-10 08:48:04 +0000

    fixed wrangler deployment

--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,5 @@
 import { createRequestHandler, type RequestHandler } from "expo-server/adapter/workerd";
 
-console.log("cwd:", new URL(".", import.meta.url).pathname);
 const handler: RequestHandler = createRequestHandler({
 	build: "./server",
 });
